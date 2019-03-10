34dccaac5104db352e4bd162a04264d777d4d950
Harmanpreet Kaur <harmanpreetkaur58@gmail.com>
2019-03-10 21:50:05 +1100

    Removing extra spaces

--- src/test/java/stepDefinition/Test.java
+++ src/test/java/stepDefinition/Test.java
@@ -22,24 +22,24 @@ public class Test {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://automationpractice.com/index.php");
     }
-    
+
     @When("^user navigates to Login Page$")
     public void user_navigates_to_Login_Page() throws Throwable {
         driver.findElement(By.linkText("Sign in")).click();
     }
-    
+
     @When("^user enters username and Password$")
     public void user_enters_username_and_Password() throws Throwable {
     	driver.findElement(By.id("email")).sendKeys("blog.cucumber@gmail.com");
         driver.findElement(By.id("passwd")).sendKeys("Cucumber@blog");
-        driver.findElement(By.id("SubmitLogin")).click();   
+        driver.findElement(By.id("SubmitLogin")).click();
     }
-    
+
     @Then("^success message is displayed$")
     public void success_message_is_displayed() throws Throwable {
-    	String exp_message = "Welcome to your account. Here you can manage all   of your personal information and orders.";
+    	String exp_message = "Welcome to your account. Here you can manage all of your personal information and orders.";
     	String actual = driver.findElement(By.cssSelector(".info-account")).getText();
         Assert.assertEquals(exp_message, actual);
-        driver.quit();  
-    }      
+        driver.quit();
+    }
 }
